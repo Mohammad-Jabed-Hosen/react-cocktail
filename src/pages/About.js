b09f3ad725771dd1d4ae965e5ed4e2c1db7cd0a3
Mohammad-Jabed-Hosen <zavedhosen66@gmail.com>
2021-09-25 00:32:36 +0600

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
+        maiores, voluptatibus laborum odit impedit ducimus. Soluta quod
+        perferendis saepe autem fugit officia iste, aut placeat sequi veritatis,
+        dolores fugiat recusandae sunt nostrum ex natus facilis labore molestias
+        ea. Eaque, possimus!
+      </p>
+    </section>
+  )
+}
+
+export default About
